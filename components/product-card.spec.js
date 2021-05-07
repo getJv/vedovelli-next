87a33bdab32b25c846f721cfab1d782aeea25353
Jhonatan Morais <jhonatanvinicius@gmail.com>
2021-05-07 12:33:54 -0300

    96

---
+++ components/product-card.spec.js
@@ -0,0 +1,24 @@
+import { screen, render } from '@testing-library/react';
+import ProductCard from './product-card';
+
+const product = {
+  title: 'Relogio bonito',
+  price: '22.00',
+  image:
+    'https://images.unsplash.com/photo-1495856458515-0637185db551?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+};
+
+describe('ProductCard', () => {
+  it('should render a ProductCard', function () {
+    render(<ProductCard product={product} />);
+    expect(screen.getByTestId('product-card')).toBeInTheDocument();
+  });
+  it('should display the proper content', function () {
+    render(<ProductCard product={product} />);
+    expect(screen.getByText(new RegExp(product.title, 'i'))).toBeInTheDocument();
+    expect(screen.getByText(new RegExp(product.price, 'i'))).toBeInTheDocument();
+    expect(screen.getByTestId('image')).toHaveStyle({
+      backgroundImage: product.image,
+    });
+  });
+});
